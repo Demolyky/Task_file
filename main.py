2e18513d2793c8210e76805ad203915dafa3ea11
Mikhail Volf <misha.volf93@gmail.com>
2022-11-07 23:18:37 +0300

    Добавлен функционал на случай, если файлы заранее известны

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import os
 
 
 def main():
-    files_info = lenght_file()
+    files = ['1.txt', '2.txt', '3.txt']
+    files_info = lenght_file(files)
     create_file_sorted_by_lenght(files_info)
 
 
-def lenght_file():
+def lenght_file(files):
     file_info = {}
-    for x, y, filename in os.walk('.'):
-        for file in filename:
-            if '.txt' in file and file != 'result.txt':
-                with open(file, 'rt') as f:
-                    file_info[file.replace('.txt', '')] = len(list(f))
+    for file in files:
+        with open(file, 'rt') as f:
+            file_info[file.replace('.txt', '')] = len(list(f))
     return file_info
 
 
